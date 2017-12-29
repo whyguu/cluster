e3899c4de71488078f278d7bd3974075179c8295
whyguu <whyguu@sina.com>
2017-12-29 16:14:46 +0800

    add print
    Signed-off-by: whyguu <whyguu@sina.com>

--- shapefile_2_tif.py
+++ shapefile_2_tif.py
@@ -3,14 +3,13 @@ import subprocess
 
 
 if __name__ == '__main__':
-    InputVector = '/usr/data/gago_yangguowei/临颍影像及地块数据/河南太保/太保.shp'
-    RefImage = '/usr/data/gago_yangguowei/临颍影像及地块数据/google_河南太保_0p6meter.tif'
-    OutputImage = '/usr/data/gago_yangguowei/临颍影像及地块数据/label_raster.tif'
-
+    InputVector = '/Users/whyguu/Desktop/临颍影像及地块数据/河南太保/line.shp'
+    RefImage = '/Users/whyguu/Desktop/临颍影像及地块数据/google_河南太保_0p6meter.tif'
+    OutputImage = 'label_raster.tif'
 
     gdalformat = 'GTiff'
     datatype = gdal.GDT_Byte
-    burnVal = 1 #value for the output image pixels
+    burnVal = 1  # value for the output image pixels
     ##########################################################
     # Get projection info from reference image
     Image = gdal.Open(RefImage, gdal.GA_ReadOnly)
@@ -19,15 +18,24 @@ if __name__ == '__main__':
     Shapefile = ogr.Open(InputVector)
     Shapefile_layer = Shapefile.GetLayer()
 
+    if not Shapefile:
+        print('shapefile not open !')
+        exit(0)
+    if not Image:
+        print('image not open !')
+        exit(0)
     # Rasterise
     print("Rasterising shapefile...")
-    Output = gdal.GetDriverByName(gdalformat).Create(OutputImage, Image.RasterXSize, Image.RasterYSize, 1, datatype, options=['COMPRESS=DEFLATE'])
+    Output = gdal.GetDriverByName(gdalformat).Create(OutputImage, Image.RasterXSize, Image.RasterYSize, 1, datatype,
+                                                     options=['COMPRESS=DEFLATE'])
     Output.SetProjection(Image.GetProjectionRef())
     Output.SetGeoTransform(Image.GetGeoTransform())
 
+    print('starting write data ...')
     # Write data to band 1
     Band = Output.GetRasterBand(1)
     Band.SetNoDataValue(0)
+    print('Band: ', Band)
     gdal.RasterizeLayer(Output, [1], Shapefile_layer, burn_values=[burnVal])
 
     # Close datasets
@@ -36,6 +44,7 @@ if __name__ == '__main__':
     Image = None
     Shapefile = None
 
+    print('building image overviews...')
     # Build image overviews
-    subprocess.call("gdaladdo --config COMPRESS_OVERVIEW DEFLATE "+OutputImage+" 2 4 8 16 32 64", shell=True)
+    subprocess.call("gdaladdo --config COMPRESS_OVERVIEW DEFLATE " + OutputImage + " 2 4 8 16 32 64", shell=True)
     print("Done.")
